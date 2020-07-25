e4535e761a749a0a73253151d4fdd62fe937918a
denis-fominykh <fominykhdenis@gmail.com>
2020-07-25 21:16:23 +0300

    connecting the screens' routing

--- App.tsx
+++ App.tsx
@@ -1,22 +1,30 @@
 import 'mobx-react-lite/batchingForReactNative';
 import 'react-native-gesture-handler';
 import React, { FC } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import WelcomePageScreen from './screens/WelcomePageScreen';
+import SignUpPageScreen from './screens/SignUpPageScreen';
+import SignInPageScreen from './screens/SignInPageScreen';
+import MainPageScreen from './screens/MainPageScreen';
+
+const Stack = createStackNavigator();
 
 const App: FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello, Addictive Learning!</Text>
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="WelcomePage" headerMode="none">
+          <Stack.Screen name="WelcomePage" component={WelcomePageScreen} />
+          <Stack.Screen name="SignUpPage" component={SignUpPageScreen} />
+          <Stack.Screen name="SignInPage" component={SignInPageScreen} />
+          <Stack.Screen name="MainPage" component={MainPageScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default App;
