238cc3fa838cd3889a5fa0a2ac8f1832b04ed89e
denis-fominykh <fominykhdenis@gmail.com>
2020-07-25 21:21:45 +0300

    adding a safe area

--- screens/SignUpPageScreen.tsx
+++ screens/SignUpPageScreen.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const SignUpPageScreen: FC = () => {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>SignUpPageScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -14,6 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#fff',
   },
 });
 
--- screens/WelcomePageScreen.tsx
+++ screens/WelcomePageScreen.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const WelcomePageScreen: FC = () => {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>WelcomePageScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -14,6 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#fff',
   },
 });
 
