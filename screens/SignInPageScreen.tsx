579b26cbbc0653b1b4a9311bc6003889cca32798
denis-fominykh <fominykhdenis@gmail.com>
2020-07-27 00:11:30 +0300

    creating a sign in page layout

--- screens/SignInPageScreen.tsx
+++ screens/SignInPageScreen.tsx
@@ -1,22 +1,58 @@
 import React, { FC } from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
+import { inject, observer } from 'mobx-react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const SignInPageScreen: FC = () => {
+import MainButton from '../components/MainButton';
+import Header from '../components/Header';
+
+interface SignInPageScreenProps {
+  navigation: any;
+}
+
+const SignInPageScreen: FC<SignInPageScreenProps> = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>SignInPageScreen</Text>
+      <Header title="Back" onPress={() => navigation.goBack()} />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          onChangeText={() => {}}
+          placeholder="email"
+          defaultValue=""
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={() => {}}
+          placeholder="password"
+          defaultValue=""
+        />
+        <MainButton onPress={() => {}} color="#6EC5D6">
+          SIGN IN
+        </MainButton>
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#FFFFFF',
+    height: '100%',
+  },
+  inputContainer: {
     alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFFFF',
+    marginTop: '50%',
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderBottomWidth: 1,
+    borderColor: '#000000',
+    marginVertical: 5,
+    fontSize: 18,
   },
 });
 
-export default SignInPageScreen;
+export default inject('store')(observer(SignInPageScreen));
