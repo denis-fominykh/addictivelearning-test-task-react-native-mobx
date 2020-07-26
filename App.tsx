5101f08b8232984c4c88144c484caba2c8c9981f
denis-fominykh <fominykhdenis@gmail.com>
2020-07-26 22:06:23 +0300

    connecting the mobx store

--- App.tsx
+++ App.tsx
@@ -4,7 +4,9 @@ import React, { FC } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'mobx-react';
 
+import { appStore } from './mobx/store';
 import WelcomePageScreen from './screens/WelcomePageScreen';
 import SignUpPageScreen from './screens/SignUpPageScreen';
 import SignInPageScreen from './screens/SignInPageScreen';
@@ -14,16 +16,18 @@ const Stack = createStackNavigator();
 
 const App: FC = () => {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="WelcomePage" headerMode="none">
-          <Stack.Screen name="WelcomePage" component={WelcomePageScreen} />
-          <Stack.Screen name="SignUpPage" component={SignUpPageScreen} />
-          <Stack.Screen name="SignInPage" component={SignInPageScreen} />
-          <Stack.Screen name="MainPage" component={MainPageScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <Provider store={appStore}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="WelcomePage" headerMode="none">
+            <Stack.Screen name="WelcomePage" component={WelcomePageScreen} />
+            <Stack.Screen name="SignUpPage" component={SignUpPageScreen} />
+            <Stack.Screen name="SignInPage" component={SignInPageScreen} />
+            <Stack.Screen name="MainPage" component={MainPageScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </Provider>
   );
 };
 
