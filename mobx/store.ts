e3b1a206fbe0a44699250deab723bad37153e563
denis-fominykh <fominykhdenis@gmail.com>
2020-07-28 18:30:27 +0300

    changing the store logic

--- mobx/store.ts
+++ mobx/store.ts
@@ -1,98 +1,126 @@
-import { observable, configure, action, runInAction } from 'mobx';
-import axios from 'axios';
+import { observable, configure, action } from 'mobx';
 
 configure({ enforceActions: 'observed' });
+
+type NewUser = {
+  email: string;
+  password: string;
+  passwordConfirm: string;
+  termsOfUse: boolean;
+};
+
+type OldUser = {
+  email: string;
+  password: string;
+};
 
 class Store {
   private apiBase: string = 'https://dev.addictivelearning.io/api/v1';
 
-  @observable email: string = '';
-  @observable password: string = '';
-  @observable passwordConfirmation: string = '';
-  @observable termsOfUse: boolean = false;
+  @observable newUser: NewUser = {
+    email: '',
+    password: '',
+    passwordConfirm: '',
+    termsOfUse: false,
+  };
 
-  @observable signUnSuccess: boolean = false;
-  @observable signInSuccess: boolean = false;
-  @observable logOutSuccess: boolean = true;
+  @observable oldUser: OldUser = {
+    email: '',
+    password: '',
+  };
+
+  @observable signInSuccess: number = 0;
 
   @action changeEmail = (email: string): void => {
-    this.email = email;
+    this.newUser.email = email;
   };
 
   @action changePassword = (password: string): void => {
-    this.password = password;
+    this.newUser.password = password;
   };
 
-  @action changePasswordConfirmation = (passwordConfirmation: string): void => {
-    this.passwordConfirmation = passwordConfirmation;
+  @action changePasswordConfirm = (passwordConfirmation: string): void => {
+    this.newUser.passwordConfirm = passwordConfirmation;
   };
 
   @action changeTermsOfUse = (termsOfUse: boolean): void => {
-    this.termsOfUse = termsOfUse;
+    this.newUser.termsOfUse = termsOfUse;
+  };
+
+  @action changeSignInEmail = (email: string) => {
+    this.oldUser.email = email;
+  };
+
+  @action changeSignInPassword = (password: string) => {
+    this.oldUser.password = password;
+  };
+
+  @action changeSignInSuccess = (value: number) => {
+    this.signInSuccess = value;
   };
 
   @action.bound
   signUp = (): void => {
     const data = new FormData();
 
-    data.append('email', this.email);
-    data.append('password', this.password);
-    data.append('password_confirmation', this.passwordConfirmation);
-    data.append('terms_of_use', this.termsOfUse);
+    data.append('email', this.newUser.email);
+    data.append('password', this.newUser.password);
+    data.append('password_confirmation', this.newUser.passwordConfirm);
+    data.append('terms_of_use', this.newUser.termsOfUse);
 
-    axios
-      .post(`${this.apiBase}/register`, {
-        data: data,
+    if (
+      this.newUser.password === this.newUser.passwordConfirm &&
+      this.newUser.termsOfUse
+    ) {
+      fetch(`${this.apiBase}/register`, {
+        method: 'POST',
+        body: data,
       })
-      .then((response) => {
-        if (response.status === 201) {
-          runInAction(() => {
-            this.signUnSuccess = true;
-          });
-          console.log(response);
-        } else {
-          console.log('Response status:', response.status);
-        }
-      })
-      .catch((error) => console.log('Error:', error));
+        .then((response) => {
+          if (response.status === 200) {
+            console.log('Sign Up:', response.status);
+          } else {
+            console.log('Response status:', response.status);
+          }
+        })
+        .catch((error) => console.log('Error:', error));
+    }
   };
 
   @action.bound
   signIn = (): void => {
     const data = new FormData();
 
-    data.append('email', this.email);
-    data.append('password', this.password);
+    data.append('email', this.oldUser.email);
+    data.append('password', this.oldUser.password);
 
-    axios
-      .post(`${this.apiBase}/login`, {
-        data: data,
+    if (this.oldUser.email && this.oldUser.password) {
+      fetch(`${this.apiBase}/login`, {
+        method: 'POST',
+        body: data,
       })
-      .then((response) => {
-        if (response.status === 201) {
-          runInAction(() => {
-            this.signInSuccess = true;
-            this.logOutSuccess = false;
-          });
-          console.log(response);
-        } else {
-          console.log('Response status:', response.status);
-        }
-      })
-      .catch((error) => console.log('Error:', error));
+        .then((response) => {
+          if (response.status === 200) {
+            this.changeSignInSuccess(response.status);
+            console.log('Sign In:', response.status);
+            console.log('Sign In Success:', this.signInSuccess);
+          } else {
+            console.log('Response status:', response.status);
+          }
+        })
+        .catch((error) => console.log('Error:', error));
+    }
   };
 
   @action.bound
   logOut = (): void => {
-    axios
-      .post(`${this.apiBase}/logout`)
+    fetch(`${this.apiBase}/logout`, {
+      method: 'POST',
+    })
       .then((response) => {
-        if (response.status === 201) {
-          runInAction(() => {
-            this.signInSuccess = false;
-            this.logOutSuccess = true;
-          });
-          console.log(response);
+        if (response.status === 200) {
+          this.changeSignInSuccess(0);
+          console.log('Log Out:', response.status);
         } else {
           console.log('Response status:', response.status);
         }
@@ -102,11 +130,12 @@ class Store {
 
   @action.bound
   getCurrentUser = (): void => {
-    axios
-      .get(`${this.apiBase}/current`)
+    fetch(`${this.apiBase}/current`, {
+      method: 'GET',
+    })
       .then((response) => {
         if (response.status === 200) {
-          console.log(response);
+          console.log('Get Current User', response.status);
         } else {
           console.log('Response status:', response.status);
         }
